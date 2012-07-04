fbcb4240e04443f21b8f8c7427665f953fafa58e
Theo Geo <theodosis8@hotmail.com>
2012-07-04 02:18:00 +0100

    test intelij

--- src/com/example/ourfirst/CameraClass.java
+++ src/com/example/ourfirst/CameraClass.java
@@ -1,143 +1,138 @@
 package com.example.ourfirst;
 
-import java.io.File;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Environment;
 import android.util.Log;
-import android.widget.ImageView;
-import android.widget.Toast;
+
+import java.io.File;
 
 @SuppressLint("ParserError")
-public class CameraClass{
-	
-	private Activity activity;
-	private String userId = "Emma"; 
-//	private String description= "efien i mparra m o yiannis ine pppouli";	
+public class CameraClass {
+
+    private Activity activity;
+    private String userId = "Emma";
+    //	private String description= "efien i mparra m o yiannis ine pppouli";
 //	private String visibility= "public;friends";
 //	private String tag= "topshop;primark";	
-	private int numPhotos = 0;
-	private static final int CAMERA_REQUEST = 1888; 
+    private int numPhotos = 0;
+    private static final int CAMERA_REQUEST = 1888;
     public String path = new String();
     public Bitmap photo;
     public static Bitmap[] photos = null;
-    
-    
-    protected CameraClass(Activity activity){
-    	InitVariables();
-    	this.activity = activity;
+
+
+    protected CameraClass(Activity activity) {
+        InitVariables();
+        this.activity = activity;
     }
-    
 
 
-	public void InitVariables(){
-    	this.photos = new Bitmap[5];
+    public void InitVariables() {
+        this.photos = new Bitmap[5];
         this.numPhotos = 0;
     }
-    
-    
-	public void LaunchCamera() {
-		// TODO Auto-generated method stub
-        this.path = Environment.getExternalStorageDirectory() + "/"  + String.valueOf(System.currentTimeMillis()) + ".jpg" ;
-    	//this.path = Environment.getDataDirectory() + "/" + String.valueOf(System.currentTimeMillis()) + ".jpg" ;
-    	Log.w("Photo Path ", this.path);         	
-    	File file = new File(path);
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);   
-        cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, file.toURI()); 
-        Log.w("Photo Path in extra ",file.toString());
-        activity.startActivityForResult(cameraIntent, CAMERA_REQUEST); 	
-     
-	}
-	
-	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
-        if (requestCode == CAMERA_REQUEST) {  
-            photo = (Bitmap) data.getExtras().get("data"); 
+
+
+    public void LaunchCamera() {
+        // TODO Auto-generated method stub
+        this.path = Environment.getExternalStorageDirectory() + "/" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        //this.path = Environment.getDataDirectory() + "/" + String.valueOf(System.currentTimeMillis()) + ".jpg" ;
+        Log.w("Photo Path ", this.path);
+        File file = new File(path);
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, file.toURI());
+        Log.w("Photo Path in extra ", file.toString());
+        activity.startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST) {
+            photo = (Bitmap) data.getExtras().get("data");
             //String backgroundUrl = MediaStore.Images.Media.insertImage(this.getContentResolver(), photo, "fashioImage", null);           
             //TODO bigger picture
-            
+
             //alert box for more images
-            Log.w("MESSAGEBOX ","Hello form msgbox");
+            Log.w("MESSAGEBOX ", "Hello form msgbox");
             msgBoxForMorePictures();
-        }  
+        }
     }
-    
-    private void msgBoxForMorePictures(){
+
+    private void msgBoxForMorePictures() {
         AlertDialog morePicMsg = new AlertDialog.Builder(this.activity).create();
         morePicMsg.setTitle("More Photos");
         morePicMsg.setMessage("Take another photo?");
-        morePicMsg.setButton("Yes", new OnClickListener(){
-            public void onClick(DialogInterface dialog, int which) {   
-            	photoInArray();
-            	LaunchCamera();
-            }
-        });
-        morePicMsg.setButton2("No", new OnClickListener(){
+        morePicMsg.setButton("Yes", new OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-            	photoInArray();
-            	Intent textIntent= new Intent(activity, InputTextActivity.class);
-            	textIntent.putExtra("numPhotos", numPhotos);
-            	textIntent.putExtra("userId", userId);
-            	activity.startActivity(textIntent); 
-            	
-            	//activity.setContentView(R.layout.activity_input_text);
-            	//sendData();
+                photoInArray();
+                LaunchCamera();
             }
         });
-        morePicMsg.show(); 
+        morePicMsg.setButton2("No", new OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                photoInArray();
+                Intent textIntent = new Intent(activity, InputTextActivity.class);
+                textIntent.putExtra("numPhotos", numPhotos);
+                textIntent.putExtra("userId", userId);
+                activity.startActivity(textIntent);
+
+                //activity.setContentView(R.layout.activity_input_text);
+                //sendData();
+            }
+        });
+        morePicMsg.show();
     }
-    
-    private void photoInArray(){
-    	File file = new File(path);
-    	photos[numPhotos] = photo;
-    	numPhotos++;
+
+    private void photoInArray() {
+        File file = new File(path);
+        photos[numPhotos] = photo;
+        numPhotos++;
     }
-    
-   /* public void sendData(String description,String visibility, String tag){
-        //imageView.setImageBitmap(photo);
-        //imageView.setVisibility(View.VISIBLE);        
-        sentClass sentclass = new sentClass(this.getUserId(),description, visibility, tag, Integer.toString(this.numPhotos), "N/A", "http://10.66.1.185:8888/mobile");       
-        try {
-        	//File file = new File(this.path);
-        	//Bitmap bmCamera = BitmapFactory.decodeFile(file.toString());           	     	
-        	//photos[0] = photo;       	
-			String sResponse = sentclass.sendData(photos); //TODO sent url to the class
-	        Log.w("Response from sentClass",sResponse);
-	        //pop up box
-	        toastMsgBox(sResponse); 
-       	 
-       	 	Intent textIntent= new Intent(this.activity, TabsActivity.class);
-        	activity.startActivity(textIntent); 
-        	
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}  	
-    	
-    }
-    
-    private void toastMsgBox(String sResponse){
-        Context context = this.activity.getApplicationContext();
-        CharSequence text = sResponse;
-        int duration = Toast.LENGTH_LONG;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-    
+
+    /* public void sendData(String description,String visibility, String tag){
+         //imageView.setImageBitmap(photo);
+         //imageView.setVisibility(View.VISIBLE);
+         sentClass sentclass = new sentClass(this.getUserId(),description, visibility, tag, Integer.toString(this.numPhotos), "N/A", "http://10.66.1.185:8888/mobile");
+         try {
+             //File file = new File(this.path);
+             //Bitmap bmCamera = BitmapFactory.decodeFile(file.toString());
+             //photos[0] = photo;
+             String sResponse = sentclass.sendData(photos); //TODO sent url to the class
+             Log.w("Response from sentClass",sResponse);
+             //pop up box
+             toastMsgBox(sResponse);
+
+                 Intent textIntent= new Intent(this.activity, TabsActivity.class);
+             activity.startActivity(textIntent);
+
+             } catch (Exception e) {
+                 // TODO Auto-generated catch block
+                 e.printStackTrace();
+             }
+
+     }
+
+     private void toastMsgBox(String sResponse){
+         Context context = this.activity.getApplicationContext();
+         CharSequence text = sResponse;
+         int duration = Toast.LENGTH_LONG;
+         Toast toast = Toast.makeText(context, text, duration);
+         toast.show();
+     }
 
 
-	public String getUserId() {
-		return userId;
-	}
-	*/
-	
-	
+
+     public String getUserId() {
+         return userId;
+     }
+     */
+
 
 }
--- src/com/example/ourfirst/MainActivity.java
+++ src/com/example/ourfirst/MainActivity.java
@@ -9,28 +9,29 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	protected boolean _active = true;
-	protected int _splashTime = 3000; // time to display the splash screen in ms
-	private Intent intent;
-	@Override
+    protected boolean _active = true;
+    protected int _splashTime = 3000; // time to display the splash screen in ms
+    private Intent intent;
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("hello","my hello");
+        Log.i("hello", "my hello");
         // thread for displaying the SplashScreen
-        intent= new Intent(this, TabsActivity.class);
+        intent = new Intent(this, TabsActivity.class);
         Thread splashTread = new Thread() {
             @Override
             public void run() {
                 try {
                     int waited = 0;
-                    while(_active && (waited < _splashTime)) {
+                    while (_active && (waited < _splashTime)) {
                         sleep(100);
-                        if(_active) {
+                        if (_active) {
                             waited += 100;
                         }
                     }
-                } catch(InterruptedException e) {
+                } catch (InterruptedException e) {
                     // do nothing
                 } finally {
                     finish();
@@ -40,44 +41,40 @@ public class MainActivity extends Activity {
             }
         };
         splashTread.start();
-        
-    }
-	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-	    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-	        _active = false;
-	    }
-	    return true;
-	}
 
-   /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            _active = false;
+        }
         return true;
     }
-*/
-   
-    
-    
-    
-   //called on AcitvityFeed button - Instead of onCLickListener
-    public void clicktabs(View view){
-    	//take picture
-    	Intent intent= new Intent(this, TabsActivity.class);
+
+    /* @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.activity_main, menu);
+            return true;
+        }
+    */
+
+
+    //called on AcitvityFeed button - Instead of onCLickListener
+    public void clicktabs(View view) {
+        //take picture
+        Intent intent = new Intent(this, TabsActivity.class);
         startActivity(intent);
-        
+
     }
-   
+
     //called on Capture button - Instead of onCLickListener
-    public void takepicture(View view){
-    	//take picture
-    	/*Intent intent= new Intent(this, TabsActivity.class);
+    public void takepicture(View view) {
+        //take picture
+        /*Intent intent= new Intent(this, TabsActivity.class);
         startActivity(intent);*/
-        
+
     }
-    
-    
-    
-    
+
+
 }
